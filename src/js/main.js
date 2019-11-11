d6a79ef8c2fb646f0d2533c5350aeeec26e74598
joeyre <joanna@starboardmedia.co.uk>
2019-11-11 16:27:53 +0000

    Remove old JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,4 @@
-// Hamburger menu JS
-
-$(document).ready(function(){
-
-    /* Menu fade/in out on mobile */
-    $(".open-button").click(function(e){
-        e.preventDefault();
-        $(this).toggleClass('open');
-        $(".menu").toggleClass('open');
-    });
-
-});
-
-$(function () {
-    $('.open-button').on('click', function (e) {
-        var menuItem = $( e.currentTarget );
-
-        if (menuItem.attr( 'aria-expanded') === 'true') {
-            $(this).attr( 'aria-expanded', 'false');
-        } else {
-            $(this).attr( 'aria-expanded', 'true');
-        }
-    });
-});
-
-// Smooth scrolling
+// Smooth scrolling when clicking on links
 
 $(document).ready(function(){
     // Add smooth scrolling to all links
